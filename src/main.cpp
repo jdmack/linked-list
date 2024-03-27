d45a83bfe743e5b1b6b9c5ccd11f4ed3320cd5c0
James <jdmack@users.noreply.github.com>
2024-03-27 16:03:28 -0700

    Create cmake project

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+Create a C++ program that implements a linked list. The linked list should be
+a doubly linked list, allowing movement forward and backward. This program
+should allow you to add and remove nodes from the list. Each node should
+contain a reference to application data. The program does not have to provide
+user interaction. Please include units tests for the program.
+*/
+
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
