cefc7a4d65f417fef59ad4e8710746ff6f024b5b
James <jdmack@users.noreply.github.com>
2024-03-27 16:03:28 -0700

    Implement insert and remove

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -1,33 +1,37 @@
 #ifndef __LINKED_LIST_H__
 #define __LINKED_LIST_H__
 
+#include <string>
 class LinkedList
 {
     class Node
     {
         public:
-            Node();
+            Node() { }
+            Node(int i) : next_(nullptr), previous_(nullptr), data_(i) { } 
             ~Node();
 
-            Node * next_;
-            Node * previous_;
-            int * data_;
+            Node * next_ {nullptr};
+            Node * previous_ {nullptr};
+            int data_ {0};
     };
 
     public:
         LinkedList();
         ~LinkedList();
 
-        bool insert(int i);
-        bool remove(int i);
-        bool lookup(int i);
+        void insert(int i);
+        void remove(int i);
+        void lookup(int i);
+
+        std::string toStr();
 
     private:
         Node * head_;
         Node * tail_;
         int occupancy_;
 
-
+        void removeNode(Node * node);
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,40 @@ a doubly linked list, allowing movement forward and backward. This program
 should allow you to add and remove nodes from the list. Each node should
 contain a reference to application data. The program does not have to provide
 user interaction. Please include units tests for the program.
+
+[X] LinkedList header
+[ ] insert()
+[ ] remove()
+[ ] lookup()
+[ ] []
+[ ] tests
+[ ] templates
+[ ] node recycling
+[ ] comments
+
+
+
+
 */
 
 #include <iostream>
+#include "LinkedList.h"
 
 int main(int argc, char ** argv)
 {
     std::cout << "Hello World" << std::endl;
+
+    LinkedList list;
+    list.insert(0);
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+
+    std::cout << list.toStr() << std::endl;
+
+    list.remove(2);
+
+    std::cout << list.toStr() << std::endl;
 
     return 0;
 }
